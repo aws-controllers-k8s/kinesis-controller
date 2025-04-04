88cb972b7d47b0b5b1c053c84c6e9d437bf3a944
Michael Tewoldemedhin <98621731+michaelhtm@users.noreply.github.com>
2025-04-04 15:04:36 -0700

    fix: propagate shardCount to resource status (#55)
    Issue [#2414 comment](https://github.com/aws-controllers-k8s/community/issues/2414#issuecomment-2778742188)
    By submitting this pull request, I confirm that my contribution is made under the terms of the Apache 2.0 license.

--- pkg/resource/stream/sdk.go
+++ pkg/resource/stream/sdk.go
@@ -315,6 +315,10 @@ func (rm *resourceManager) sdkUpdate(
 	}
 
 	rm.setStatusDefaults(ko)
+	if resp.TargetShardCount != nil {
+		ko.Status.OpenShardCount = aws.Int64(int64(*resp.TargetShardCount))
+	}
+
 	return &resource{ko}, nil
 }
 
